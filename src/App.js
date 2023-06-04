0827ec329dee53c7cd17abac4a92553254d7b89a
prakharsrivastav09 <prakharsrivastav45@gmail.com>
2023-06-05 00:39:47 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,109 +1,171 @@
-import { useState, useEffect } from "react";
-import './input.css';
+import {useState, useEffect, useRef} from 'react'
 import randomWords from 'random-words'
-const WORDS = 200
-
-const STIME = 10
+const NUMB_OF_WORDS = 200
+const SECONDS = 60
 
 function App() {
-  const[words, setWords] = useState([])
-  const[countDown, setCountDown] = useState(STIME)
+  const [words, setWords] = useState([])
+  const [countDown, setCountDown] = useState(SECONDS)
+  const [currInput, setCurrInput] = useState("")
+  const [currWordIndex, setCurrWordIndex] = useState(0)
+  const [currCharIndex, setCurrCharIndex] = useState(-1)
+  const [currChar, setCurrChar] = useState("")
+  const [correct, setCorrect] = useState(0)
+  const [incorrect, setIncorrect] = useState(0)
+  const [status, setStatus] = useState("waiting")
+  const textInput = useRef(null)
 
-  useEffect(()=>{
+  useEffect(() => {
     setWords(generateWords())
   }, [])
 
+  useEffect(() => {
+    if (status === 'started') {
+      textInput.current.focus()
+    }
+  }, [status])
+
   function generateWords() {
-    return new Array(WORDS).fill(null).map(()=> randomWords())
-
-
+    return new Array(NUMB_OF_WORDS).fill(null).map(() => randomWords())
   }
+
   function start() {
-   let interval = setInterval(()=>{
-      setCountDown((prevCountDown) => {
-        if(prevCountDown===0){
-          clearInterval(interval)
-        } else{
-          return prevCountDown - 1
 
-        }
-        
+    if (status === 'finished') {
+      setWords(generateWords())
+      setCurrWordIndex(0)
+      setCorrect(0)
+      setIncorrect(0)
+      setCurrCharIndex(-1)
+      setCurrChar("")
+    }
 
-      } )
-
-    }, 1000)
+    if (status !== 'started') {
+      setStatus('started')
+      let interval = setInterval(() => {
+        setCountDown((prevCountdown) => {
+          if (prevCountdown === 0) {
+            clearInterval(interval)
+            setStatus('finished')
+            setCurrInput("")
+            return SECONDS
+          } else {
+            return prevCountdown - 1
+          }
+        }  )
+      } ,  1000 )
+    }
+    
   }
 
-  function handleKeyDown(event){
-    console.log(event.key)
+  function handleKeyDown({keyCode, key}) {
+    // space bar 
+    if (keyCode === 32) {
+      checkMatch()
+      setCurrInput("")
+      setCurrWordIndex(currWordIndex + 1)
+      setCurrCharIndex(-1)
+    // backspace
+    } else if (keyCode === 8) {
+      setCurrCharIndex(currCharIndex - 1)
+      setCurrChar("")
+    } else {
+      setCurrCharIndex(currCharIndex + 1)
+      setCurrChar(key)
+    }
   }
 
+  function checkMatch() {
+    const wordToCompare = words[currWordIndex]
+    const doesItMatch = wordToCompare === currInput.trim()
+    if (doesItMatch) {
+      setCorrect(correct + 1)
+    } else {
+      setIncorrect(incorrect + 1)
+    }
+  }
+
+  function getCharClass(wordIdx, charIdx, char) {
+    if (wordIdx === currWordIndex && charIdx === currCharIndex && currChar && status !== 'finished') {
+      if (char === currChar) {
+        return 'has-background-success'
+      } else {
+        return 'has-background-danger'
+      }
+    } else if (wordIdx === currWordIndex && currCharIndex >= words[currWordIndex].length) {
+      return 'has-background-danger'
+    } else {
+      return ''
+    }
+  }
+
+
+  
 
 
   return (
+
+    
     <div className="App">
+      
       <div className="section">
         <div className="is-size-1 has-text-centered has-text-primary">
           <h2>{countDown}</h2>
-
         </div>
-
-      <div className="card">
-        <div className="card-content">
-        
-          <div className="content">
-  
-            {words.map((word, i)=>(
-              <>
-              <span key ={i}>
-                {word.split("").map((char, idx) =>(
-                  <span key={idx}>{char}</span>
-
-                ))}
-              </span>
-              <span> </span>
-
-              </>
-
-         
-
-            ))}
-
-            <div className="control is-expanded section">
-
-      <label class = "col-form-label col-form-label-lg" ></label>
-
-      <input id = "input-typing" type = "text" className="input"onKeyDown={handleKeyDown} autoCorrect="off" autoCapitalize="none"
-      placeholder="Re-type if failed, press <TAB> or <ESC> to resest" 
-      spellCheck = "false" class="form-control form-control-lg" >
-      </input>
-    </div>
-
-    
-
-    <div className="section">
-    <button className="button is-info is-fullwidth" onClick={start}>
-      Next
-    </button>
-    </div>
-
-    <div class= "center stats mt-auto">
-      <h4>WPM:33</h4>
-      &nbsp; &nbsp; &nbsp; &nbsp;
-      <h4>Accuracy:100%</h4>
-      &nbsp; &nbsp; &nbsp; &nbsp;
-      <h4>Average WPM:0</h4>
-    </div>
-            
-            </div>
-        </div>
-        </div>
-
-
       </div>
-    </div>
-            
+      
+      <div className="control is-expanded section">
+        <input ref={textInput} disabled={status !== "started"} type="text" className="input" onKeyDown={handleKeyDown} value={currInput} onChange={(e) => setCurrInput(e.target.value)}  />
+      </div>
+      <div className="section">
+        <button className="button is-info is-fullwidth" onClick={start}>
+          Start
+        </button>
+      </div>
+      {status === 'started' && (
+        <div className="section" >
+          <div className="card">
+            <div className="card-content">
+              <div className="content">
+                {words.map((word, i) => (
+                  <span key={i}>
+                    <span>
+                      {word.split("").map((char, idx) => (
+                        <span className={getCharClass(i, idx, char)} key={idx}>{char}</span>
+                      )) }
+                    </span>
+                    <span> </span>
+                  </span>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+      {status === 'finished' && (
+        <div className="section">
+          <div className="columns">
+            <div className="column has-text-centered">
+              <p className="is-size-5">Words per minute:</p>
+              <p className="has-text-primary is-size-1">
+                {correct}
+              </p>
+            </div>
+            <div className="column has-text-centered">
+              <p className="is-size-5">Accuracy:</p>
+              {correct !== 0 ? (
+                <p className="has-text-info is-size-1">
+                  {Math.round((correct / (correct + incorrect)) * 100)}%
+                </p>
+              ) : (
+                <p className="has-text-info is-size-1">0%</p>
+              )}
+            </div>
+          </div>
+        </div>
+      )}
 
+    </div>
   );
 }
 
